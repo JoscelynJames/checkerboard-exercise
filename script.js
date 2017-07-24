a916eb013b7b26405fb6d2cdc138808450746d4c
Joscelyn James <josce.james7@hotmail.com>
2017-07-24 11:49:18 -0600

    random colors

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// Your JS goes here
+var row = Math.round(document.body.clientWidth / 11.1);
+var col = Math.round(window.innerHeight / 11.1);
+
+function getRandomColor(){
+  var color='';
+
+  while(color.length<6) {
+    color = Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  return '#'+color;
+};
+
+  for (var i = 0; i < (row + col); i++) {
+    if (i % 2 === 0) {
+    var div = document.createElement('div');
+      document.body.appendChild(div)
+        div.className = 'checkerboard';
+        div.style.width = '11.1%';
+        div.style.float = 'left';
+        div.style.paddingBottom = '11.1%';
+        div.style.backgroundColor = getRandomColor();
+        div.style.backgroundRepeat = 'repeat';
+    } else {
+      var div = document.createElement('div');
+        document.body.appendChild(div)
+          div.className = 'checkerboard';
+          div.style.width = '11.1%';
+          div.style.float = 'left';
+          div.style.paddingBottom = '11.1%';
+          div.style.backgroundColor = getRandomColor();
+          div.style.backgroundRepeat = 'repeat';
+    }
+  }
